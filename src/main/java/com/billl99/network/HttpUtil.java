40583434e25c63346eb77816daa93ebd37347963
shieryujian <709187743@qq.com>
2018-11-30 10:40:27 +0800

    http工具类

---
+++ src/main/java/com/billl99/network/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.billl99.network;/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2018 XXX, Inc. All rights reserved. <p>
+ * Company: 快钱<p>
+ *
+ * @author yong.shi.nj
+ * @since 2018/11/30 10:39
+ */
+
+/**
+ *@ClassName HttpUtil
+ *@Description TODO
+ *@Author yong.shi.nj
+ *@Date 2018/11/30 10:39
+ *@Version 1.0
+ **/
+public class HttpUtil {
+}
